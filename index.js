4d19005974e5e4f8a7089ddd73edaa9c76e61572
nxzᶻˣ <ondarkbr@gmail.com>
2026-01-13 11:36:02 -0300

    Refactor temperature conversion logic and error handling

--- index.js
+++ index.js
@@ -5,7 +5,18 @@ const result = document.getElementById("result");
 
 function convert() {
     const temp = textBox.value
-    if (toFahrenheit.checked) {
+    if (isNaN(temp)) {
+        result.textContent = "Por favor, insira um valor numérico!";
+    }
+    if(temp == ""){
+        if(toFahrenheit.checked){
+            result.textContent = "0°C é igual a 32°F (nada é igual a 0)"
+        }
+        else if(toCelsius.checked){
+            result.textContent = "0°F é igual a -17.77777777777778°C (nada é igual a 0)"
+        }
+    }
+    else if (toFahrenheit.checked) {
         let f = (temp * 1.8) + 32;
         result.textContent = `${temp}°C é igual a ${f}°F`;
     }
@@ -16,18 +27,8 @@ function convert() {
     else if (!toFahrenheit.checked && !toCelsius.checked){
         result.textContent = "Você prescisa selecionar uma opção!";
     }
-    else if(temp == NaN && toFahrenheit.checked || toCelsius.checked || temp == "" && toFahrenheit.checked || toCelsius.checked){
-        result.textContent = "Você deve colocar um número";
-    }
-    else if(temp == ""){
-        if(toFahrenheit.checked){
-            result.textContent = "0°C é igual a 32°F"
-        }
-        if(toCelsius.checked){
-            result.textContent = "0°F é igual a -17.77777777777778°C"
-        }
-    }
 }
+
 
 
 
